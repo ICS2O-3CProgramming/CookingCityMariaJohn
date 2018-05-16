c776b2069049a4245d647044aea49a8313d54a8e
John Ngundeng <john.ngundeng@ocsbstudent.ca>
2018-05-16 11:54:48 -0400

    made it work

--- level1_screen.lua
+++ level1_screen.lua
@@ -34,14 +34,17 @@ local pizzadough
 local carrot    
 local cheese
 local chocolate
-local peperonibag
+local peperoni
 local saucepacket
 local strawberry
 local tomato
 local sauce
 local shreddedcheese
+local peperonislices
 -- text for level1
 local level1text
+local lives = 3
+local questionsAnswered = 0
 -----------------------------------------------------------------------------------------
 -- GLOBAL SCENE FUNCTIONS
 -----------------------------------------------------------------------------------------
@@ -49,6 +52,10 @@ local level1text
 
 function ResumeGame()
 
+end
+
+local function YouLoseTransition()
+    composer.gotoScene( "you_lose" )
 end
 
 --checking to see if the user pressed the right answer and bring them back to level 1
@@ -60,6 +67,7 @@ local function TouchListenerResume(touch)
     
     end 
 end
+
 
 -- Creating Transition to help Screen
 local function OptionScreen( )
@@ -73,7 +81,112 @@ local function HowToScreen( )
     composer.showOverlay( "howto_screen", { isModal = true, effect = "fade", time = 100})
 end 
 
--- idea was the widget thing like the back button 
+-- Creating Transition to help Screen
+local function SaucePacketButton()
+    -- show overlay with math question
+    if (questionsAnswered == 0) then
+        -- make the sauce packet invisible
+        saucepacket.isVisible = false
+
+        -- make the sauce visible over top of the pizza
+        sauce.isVisible = true
+
+        -- adding to the score
+        questionsAnswered = questionsAnswered  + 1 
+    else
+        lives = lives - 1
+        
+        if (lives == 0) then
+            timer.performWithDelay(200, YouLoseTransition)
+        end
+    end
+end 
+
+-- Creating Transition to help Screen
+local function CheeseButton()
+    -- show overlay with math question
+    if (questionsAnswered == 1) then
+        -- make the sauce packet invisible
+        cheese.isVisible = false
+
+        -- make the sauce visible over top of the pizza
+        shreddedcheese.isVisible = true
+
+        -- adding to the score
+        questionsAnswered = questionsAnswered  + 1 
+    else
+        lives = lives - 1
+        
+        if (lives == 0) then
+            timer.performWithDelay(200, YouLoseTransition)
+        end
+    end
+end
+
+-- Creating Transition to help Screen
+local function PeperoniButton()
+    -- show overlay with math question
+    if (questionsAnswered == 2) then
+        -- make the sauce packet invisible
+        peperoni.isVisible = false
+
+        -- make the sauce visible over top of the pizza
+        peperonislices.isVisible = true
+
+        -- adding to the score
+        questionsAnswered = questionsAnswered  + 1 
+    else
+        lives = lives - 1
+        
+        if (lives == 0) then
+            timer.performWithDelay(200, YouLoseTransition)
+        end
+    end
+end
+
+-- Creating Transition to help Screen
+local function CarrotButton()
+    -- show overlay with math question
+    lives = lives - 1
+    
+    if (lives == 0) then
+    
+        timer.performWithDelay(200, YouLoseTransition)
+    end
+end
+
+-- Creating Transition to help Screen
+local function StrawberryButton()
+    -- show overlay with math question
+    lives = lives - 1
+    
+    if (lives == 0) then
+    
+        timer.performWithDelay(200, YouLoseTransition)
+    end
+end
+-- Creating Transition to help Screen
+local function ChocolateButton()
+
+    lives = lives - 1    
+    
+    if (lives == 0) then
+    
+     timer.performWithDelay(200, YouLoseTransition)
+    
+    end
+end
+
+-- Creating Transition to help Screen
+local function TomatoButton()
+    -- show overlay with math question
+    lives = lives - 1
+    
+    if (lives == 0) then
+    
+        timer.performWithDelay(200, YouLoseTransition)
+    end
+end
 
 -- The function called when the screen doesn't exist
 function scene:create( event )
@@ -94,35 +207,40 @@ function scene:create( event )
     pizzadough = display.newImageRect("L1images/pizzadough.png", 350, 350 )
     pizzadough.x = display.contentCenterX
     pizzadough.y = display.contentCenterY/1.2
+
+    -- Insert the image
+    sauce = display.newImageRect("L1images/Sauce@2x.png", 350, 350 )
+    sauce.x = display.contentCenterX
+    sauce.y = display.contentCenterY/1.2
+    sauce.isVisible = false
+
+    -- Insert the image
+    shreddedcheese = display.newImageRect("L1images/ShreddedCheese@2x.png", 350, 350 )
+    shreddedcheese.x = display.contentCenterX
+    shreddedcheese.y = display.contentCenterY/1.2
+    shreddedcheese.isVisible = false
+
+    -- Insert the image
+    peperonislices = display.newImageRect("L1images/peperonislices@2x.png", 350, 350 )
+    peperonislices.x = display.contentCenterX
+    peperonislices.y = display.contentCenterY/1.2
+    peperonislices.isVisible = false
     
     -- Insert the image
     carrot = display.newImageRect("L1images/carrot.png", 200, 88 )
-    carrot.x = display.contentCenterX*.7
+    carrot.x = display.contentCenterX*.6
     carrot.y = display.contentCenterY/2*3.3
     
-    -- Insert the image
-    cheese = display.newImageRect("L1images/Cheese.png", 100, 100 )
-    cheese.x = display.contentCenterX*1.6
-    cheese.y = display.contentCenterY/2*3.72
     
     -- Insert the image
     chocolate = display.newImageRect("L1images/Chocolate.png", 180, 68 )
-    chocolate.x = display.contentCenterX*1.3
+    chocolate.x = display.contentCenterX*1.5
     chocolate.y = display.contentCenterY/2*3.3
     
-    -- Insert the image
-    peperonibag = display.newImageRect("L1images/peperonibag.png", 100, 100 )
-    peperonibag.x = display.contentCenterX*.445
-    peperonibag.y = display.contentCenterY/2*3.72
-    
-    -- Insert the image
-    saucepacket = display.newImageRect("L1images/SaucePacket.png", 100, 100 )
-    saucepacket.x = display.contentCenterX
-    saucepacket.y = display.contentCenterY/2*3.72
     
     -- Insert the image
     strawberry = display.newImageRect("L1images/Strawberry.png", 100, 100 )
-    strawberry.x = display.contentCenterX*.7
+    strawberry.x = display.contentCenterX*.445
     strawberry.y = display.contentCenterY/2*3.72
     
     -- Insert the  image
@@ -135,6 +253,40 @@ function scene:create( event )
     --set the initial x and y position of the text
     level1text.x = 500
     level1text.y = display.contentCenterY/6
+
+    -- Creating pause Button
+    strawberry = widget.newButton( 
+        {   
+            -- Set its position on the screen relative to the screen size
+            x = display.contentCenterX*1.3,
+            y = display.contentCenterY/2*3.72,
+
+            -- Insert the images here
+            defaultFile = "L1images/Strawberry.png",
+            overFile = "L1images/Strawberry.png",
+
+
+
+            -- When the button is released, call the Level1 screen transition function
+            onRelease = StrawberryButton          
+        } )
+
+    -- Creating pause Button
+    tomato = widget.newButton( 
+        {   
+            -- Set its position on the screen relative to the screen size
+            x = display.contentCenterX*1.3,
+            y = display.contentCenterY/2*3.72,
+
+            -- Insert the images here
+            defaultFile = "L1images/tomato.png",
+            overFile = "L1images/tomato.png",
+
+
+
+            -- When the button is released, call the Level1 screen transition function
+            onRelease = TomatoButton          
+        } )
     
     -- Creating pause Button
     Pause = widget.newButton( 
@@ -170,6 +322,57 @@ function scene:create( event )
             onRelease = HowToScreen          
         } )
 
+    -- Creating pause Button
+    saucepacket = widget.newButton( 
+        {   
+            -- Set its position on the screen relative to the screen size
+            x = display.contentCenterX*.8,
+            y = display.contentCenterY/2*3.72,
+
+            -- Insert the images here
+            defaultFile = "L1images/SaucePacket.png",
+            overFile = "L1images/SaucePacket.png",
+
+
+
+            -- When the button is released, call the Level1 screen transition function
+            onRelease = SaucePacketButton          
+        } )
+
+    -- Creating pause Button
+    cheese = widget.newButton( 
+        {   
+            -- Set its position on the screen relative to the screen size
+            x = display.contentCenterX*1.6,
+            y = display.contentCenterY/2*3.72,
+
+            -- Insert the images here
+            defaultFile = "L1images/Cheese.png",
+            overFile = "L1images/Cheese.png",
+
+
+
+            -- When the button is released, call the Level1 screen transition function
+            onRelease = CheeseButton          
+        } )
+
+    -- Creating pause Button
+    peperoni = widget.newButton( 
+        {   
+            -- Set its position on the screen relative to the screen size
+            x = display.contentCenterX*1.09,
+            y = display.contentCenterY/2*3.3,
+
+            -- Insert the images here
+            defaultFile = "L1images/peperoni.png",
+            overFile = "L1images/peperoni.png",
+
+
+
+            -- When the button is released, call the Level1 screen transition function
+            onRelease = PeperoniButton          
+        } )
+
     
 
     -- Send the background image to the back layer so all other objects can be on top
@@ -182,12 +385,15 @@ function scene:create( event )
     sceneGroup:insert( saucepacket )
     sceneGroup:insert( chocolate )
     sceneGroup:insert( carrot )
-    sceneGroup:insert( peperonibag )
+    sceneGroup:insert( peperoni )
     sceneGroup:insert( cheese )
     sceneGroup:insert( tomato )
     sceneGroup:insert( Pause )
     sceneGroup:insert( level1text ) 
     sceneGroup:insert( HowTo )
+    sceneGroup:insert( sauce )
+    sceneGroup:insert( shreddedcheese )
+    sceneGroup:insert( peperonislices )
 
 end --function scene:create( event )
 
